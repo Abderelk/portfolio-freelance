26d34b750f95a5cddab24c0d56d1c1c932887786
Abderelk <abderrahmanelkafif@gmail.com>
2026-03-08 22:32:19 +0100

    add files for referency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
-import "./globals.css";
+import "globals";
 
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
